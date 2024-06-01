bc490623012317882d352b8fe7a542ad777aaeb6
regenmantel <philipstedronsky@gmail.com>
2024-06-01 21:19:32 +0200

    get json file (temp)

---
+++ GolbalVariables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wurzelbot
+{
+    public static class GlobalVariables
+    {
+        public static string JsonFilePath { get; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Storage\Data\coordinates.json");
+        public static string JsonData { get; } = File.ReadAllText(JsonFilePath);
+    }
+}
