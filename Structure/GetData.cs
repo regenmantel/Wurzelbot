d5186875a196fc349639774e721aa0973ded28dc
regenmantel <philipstedronsky@gmail.com>
2024-06-01 21:17:27 +0200

    added get/set for json data

---
+++ Structure/GetData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wurzelbot.Structure
+{
+    public class GetData
+    {
+        public int startX {  get; set; }
+        public int startY { get; set; }
+        public int endX { get; set; }
+        public int endY { get; set; }
+        public int plantX { get; set; }
+        public int plantY { get; set; }
+        public int harvestX { get; set; }
+        public int harvestY { get; set; }
+        public int waterX { get; set; }
+        public int waterY { get; set; }
+    }
+}
