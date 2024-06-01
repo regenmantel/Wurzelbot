5d9dd3134588c30f4d51d0ee14a4c50b8f4fe740
regenmantel <philipstedronsky@gmail.com>
2024-06-01 21:16:02 +0200

    added base for json data

---
+++ Structure/Base.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wurzelbot.Structure
+{
+    public class Base
+    {
+        public GetData? Normal { get; set; }
+        public GetData? Bigger {  get; set; }
+    }
+}
